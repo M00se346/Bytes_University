6ba1a35811abef616c8d877910ecb33cbbcbb6c3
anikkasood <asood008@ucr.edu>
2024-11-16 10:10:56 -0800

    add initial home page template

---
+++ bytes-uni/src/components/Home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React page template.</p>
+    </div>
+  );
+}
+
+export default HomePage;
